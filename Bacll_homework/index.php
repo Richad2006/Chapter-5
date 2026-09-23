b903d25308c17b4878609594ddb113ad5062403f
Richad2006 <richardpov377@gmail.com>
2026-09-23 10:14:29 +0700

    Create index.php

---
+++ Bacll_homework/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Select Class</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="header">
+                <h1>Select Form</h1>
+                <p>Welcome to our calcaute grade for your Bacll exam score</p>
+
+                <h4>Select your class (Science or Socail Science)</h4>
+            </div>
+
+            <div class="btn">
+                <button class="science-btn">
+                    <a href="science.php">Science Class</a>
+                </button>
+
+                <button class="social-btn">
+                    <a href="social.php">Social Science Class</a>
+                </button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
